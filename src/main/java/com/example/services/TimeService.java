cda97cf03773cb2065a21e368fbd8f6bca1f2379
qnq777 <qnq777-noreply.example.com>
2012-10-19 23:34:21 +0900

    add parameter to rest-service

--- src/main/java/com/example/services/TimeService.java
+++ src/main/java/com/example/services/TimeService.java
@@ -1,15 +1,24 @@
 package com.example.services;
 
+import java.util.TimeZone;
+
 import com.example.models.Time;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/time")
 @Produces(MediaType.APPLICATION_JSON)
 public class TimeService {
+
+    @GET
+    @Path("/{timezone}")
+    public Time get(@PathParam("timezone") String timezone) {
+        return new Time(TimeZone.getTimeZone(timezone.toUpperCase()));
+    }
 
     @GET
     public Time get() {
